2405dbd8a8d00f85a1c6fafae3faa5adc6b95160
yxue <xueyishu2436@gmail.com>
2022-06-26 19:47:20 -0700

    Add example dataset

---
+++ R/ggshotchart.R
@@ -0,0 +1,22 @@
+#'Shot Chart
+#'
+#' @description Draw a shot chart on court.
+#'
+#' @details Shot charts helps provide insights when analyzing a player's strength
+#'   and weakness.
+#' @param shotdata a data.frame containing the location of shots.
+#' @param x,y x and y variables for drawing
+#' @param color optional, color for shot locations
+#' @param shape optional, shape for shot locations
+#'
+#' @seealso
+#' @examples
+#'
+#'
+#' @export
+ggshotchart <- function(shotdata, x, y,
+                        orientation = "wide",
+                        bytype = FALSE,
+                        ...) {
+
+}
---
+++ R/shotdata.R
@@ -0,0 +1,18 @@
+#'Shot data for selected players.
+#'
+#' @description A `data.frame` containing the shot attempts made by 191 players
+#'   during the 2017--2018 NBA regular season (overtime periods not included).
+#' @name shotdata
+#' @docType data
+#' @usage data("shotdata")
+#' @format a data.frame with columns being: player name, indicator for
+#'   made/missed shots, shot type, distance, x and y locations of shot, and
+#'   string describing made/missed shot results
+#'
+#' @references \url{https://onlinelibrary.wiley.com/doi/abs/10.1002/sta4.324}
+#'
+#' @examples
+#' data(shotdata)
+#' # Default plot
+#'
+NULL
