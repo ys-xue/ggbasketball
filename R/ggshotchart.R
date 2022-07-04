1188660b5b7050711637ddad11a23a7196abf061
ys-xue <yishu.xue@uconn.edu>
2022-07-04 08:47:57 -0700

    add ggshotchart example in readme

--- R/ggshotchart.R
+++ R/ggshotchart.R
@@ -9,6 +9,8 @@
 #'   court, and should have values between [0, 47]. y aligns with the narrower
 #'   side of the court (facing the hoop), and should have values between
 #'   [-25, 25], with the hoop corresponding to a y value of 0.
+#' @param orientation orientation of the court, defaults to "wide"; options are
+#'   "wide" and "tall".
 #' @param result optional, if not FALSE, it should point to a column indicating
 #'   whether the shot was made or missed. Both point shape and color will be
 #'   varied by the result.
@@ -20,16 +22,18 @@
 #' harden <- shotdata[shotdata$Name == "James Harden", ]
 #' head(harden)
 #' # A Basic Shotchart
-#'
 #' ggshotchart(harden, x = "loc_x", y = "loc_y")
 #'
 #' # Change orientation
 #' ggshotchart(harden, x = "loc_x", y = "loc_y", orientation = "tall")
+#'
+#' # vary the color and shape by shot result
+#' ggshotchart(harden, x = "loc_x", y = "loc_y", result = "result_str")
+#'
 #' @export
 ggshotchart <- function(shotdata, x, y,
                         orientation = "wide",
-                        bytype = FALSE,
-                        result,
+                        result = NULL,
                         ...) {
 
   mapping <- ggpubr::create_aes(list(x = x, y = y, shape = result, col = result), parse = TRUE)
--- README.md
+++ README.md
@@ -18,3 +18,19 @@ ggcourt(orientation = "wide")
 ```
 
 ![](tools/README-ggbasketball-1.png)
+
+## Shotchart
+
+```r
+# simple shot chart
+data(shotdata)
+harden <- shotdata[shotdata$Name == "James Harden", ]
+ggshotchart(harden, x = "loc_x", y = "loc_y")
+```
+![](tools/README-ggbasketball-2.png)
+
+```r
+#' # vary the color and shape by shot result
+#' ggshotchart(harden, x = "loc_x", y = "loc_y", result = "result_str")
+```
+![](tools/README-ggbasketball-3.png)
